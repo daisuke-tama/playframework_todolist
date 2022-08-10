248601c1553f437ba7c3f524f4527b8aaf1f961a
daisuke-tama <smithhearth.dr@gmail.com>
2022-08-10 15:11:28 +0900

    タスクの一覧表示、新規登録、削除を可能にする

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,12 +5,16 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.db.Database
 import play.api.i18n.I18nSupport
+import anorm._
+import anorm.SqlParser._
+import scala.language.postfixOps
 
 import models.Task
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
+class HomeController @Inject()(db: Database, val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
   val taskForm = Form(
     "label" -> nonEmptyText
   )
@@ -20,18 +24,32 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   }
 
   def tasks = Action { implicit request =>
-  Ok(views.html.index(Task.all(), taskForm))
+  Ok(views.html.index(all(), taskForm))
+  }
+  def all(): List[Task] = db.withConnection { implicit c =>
+    SQL("select * from task").as(Task.task *)
   }
 
   def newTask = Action { implicit request =>
     taskForm.bindFromRequest.fold(
-      errors => BadRequest(views.html.index(Task.all(), errors)),
+      errors => BadRequest(views.html.index(all(), errors)),
       label => {
-        Task.create(label)
+        db.withConnection { implicit c =>
+          SQL("insert into task (label) values ({label})").on(
+            'label -> label
+          ).executeUpdate()
+        }
         Redirect(routes.HomeController.tasks)
       }
     )
   }
 
-  def deleteTask(id: Long) = TODO
+  def deleteTask(id: Long) = Action {
+    db.withConnection { implicit c =>
+      SQL("delete from task where id = {id}").on(
+        'id -> id
+      ).executeUpdate()
+    }
+    Redirect(routes.HomeController.tasks)
+  }
 }
--- app/models/task.scala
+++ app/models/task.scala
@@ -1,11 +1,15 @@
 package models
 
+import anorm._
+import anorm.SqlParser._
+
 case class Task(id: Long, label: String)
 
 object Task {
-  def all(): List[Task] = Nil
-
-  def create(label: String) = {}
-
-  def delete(id: Long) = {}
+  val task = {
+    get[Long]("id") ~ 
+    get[String]("label") map {
+      case id~label => Task(id, label)
+    }
+  }  
 }
