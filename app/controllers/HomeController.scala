39dd0bc7f8d1666b6a3f787fd9e2dbf20c7c319d
daisuke-tama <smithhearth.dr@gmail.com>
2022-08-10 14:55:02 +0900

    タスクのフォーム送信を可能にする

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,22 +3,35 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import models.Task
+
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Redirect(routes.HomeController.tasks)
   }
+
+  def tasks = Action { implicit request =>
+  Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.HomeController.tasks)
+      }
+    )
+  }
+
+  def deleteTask(id: Long) = TODO
 }
